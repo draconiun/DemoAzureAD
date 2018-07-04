749b3b105bb8abb8e7136417db0539f2f91be304
Aarón Mejía Alfaro <aaronmejialfaro@gmail.com>
2018-07-04 00:52:23 -0500

    first commit

---
+++ DemoAzureAD/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoAzureAD.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var displayName = identity.Claims.First(c => c.Type == "name").Value;
+            ViewBag.NameAuthentication = displayName;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
